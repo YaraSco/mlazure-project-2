8a8331e253591c5eaedb76e45029fc40a44fa403
YaraSco <53176263+YaraSco@users.noreply.github.com>
2022-05-25 04:26:31 +0000

    Update benchmark.sh

--- starter_files/benchmark.sh
+++ starter_files/benchmark.sh
@@ -70,4 +70,4 @@
 # Total:        114  160  28.0    172     199#
 
 
- ab -n 10 -v 4 -p data.json -T 'application/json' -H 'Authorization: Bearer REPLACE_WITH_KEY' http://REPLACE_WITH_API_URL/score
+ ab -n 10 -v 4 -p data.json -T 'application/json' -H 'Authorization: Bearer RdD0wlqle8RMysCwaHb1jLFPeacDpEas' http://bcd82997-52a6-4c75-bd03-10f6c77bea30.southcentralus.azurecontainer.io/score
